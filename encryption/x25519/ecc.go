90c787ebdc6c5933bae9b7621a92c43cbddcef71
ykm11 <ushiromiya3@gmail.com>
2019-01-30 09:39:48 +0900

    add Export/Import ECC Key

--- encryption/x25519/ecc.go
+++ encryption/x25519/ecc.go
@@ -57,6 +57,35 @@ func Check(p Position) int {
     return l.Cmp(r)
 }
 
+func Export_PubKey(p Position) []byte {
+    /*
+        RFC5480
+        | \x04 || X.Bytes() || Y.Bytes() | == 65; (1 + 32 + 32)
+    */
+    key := make([]byte, 65)
+    key[0] = '\x04'
+    x := p.X.Bytes()
+    y := p.Y.Bytes()
+    for j := 0; j < len(x); j++ {
+        key[32 - j] = x[31 - j]
+    }
+    for j := 0; j < len(y); j++ {
+        key[32*2 - j] = y[31 - j]
+    }
+    return key
+}
+
+func Import_PubKey(key []byte) Position {
+    if (len(key) != 65) {
+        fmt.Printf("[FAATL] key length : %d\n", len(key))
+        fmt.Printf("[FATAL] key : % x\n", key)
+        panic("ECC PubKey Importing Error")
+    }
+    x := new(big.Int).SetBytes(key[1:1+32])
+    y := new(big.Int).SetBytes(key[1+32:])
+    return Position{x, y}
+}
+
 func EC_Add(p1, p2 Position) Position {
     x2_x1 := sub(p2.X, p1.X)
     y2_y1 := sub(p2.Y, p1.Y)
